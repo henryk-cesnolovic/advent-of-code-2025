5030b59d4e2f1ef92dce1df5ad12af020c71e364
Henryk Cesnolovic <unxe@Henryks-MacBook-Pro.local>
2025-12-05 23:54:18 +0100

    Day 5 solution

---
+++ d5/solution.scala
@@ -0,0 +1,85 @@
+import scala.io.Source
+
+def time[T](block: => T): T = {
+  val before = System.nanoTime
+  val result = block
+  val after = System.nanoTime
+  println(s"Elapsed time: ${(after - before) / 1000000}ms. Result: $result")
+  result
+}
+
+@main
+def d1() =
+  val data =
+    Source.fromFile("data.txt").getLines().to(List)
+  val dataTest =
+    Source.fromFile("data_test.txt").getLines().to(List)
+
+  time(solution1(dataTest))
+  time(solution1(data))
+
+  time(solution2(dataTest))
+  time(solution2(data))
+
+// check each ingridient if it falls to atleast one range
+def solution1(data: List[String]): Long =
+  val splitIndex = data.lastIndexOf("")
+  val twoParts = data.splitAt(splitIndex)
+
+  val ranges =
+    twoParts._1.map(_.split("-")).map(row => (row(0).toLong, row(1).toLong))
+  val ingridients = twoParts._2.drop(1).map(_.toLong)
+
+  ingridients.foldLeft(0L) { case (acc, ingridient) =>
+    if (
+      ranges.filter { range =>
+        ingridient >= range._1 && ingridient <= range._2
+      }.size > 0
+    )
+      acc + 1
+    else
+      acc
+  }
+
+// try merge overlap ranges and sum differences between range end and start
+def solution2(data: List[String]): Long =
+  val splitIndex = data.lastIndexOf("")
+  val twoParts = data.splitAt(splitIndex)
+
+  val ranges =
+    twoParts._1.map(_.split("-")).map(row => (row(0).toLong, row(1).toLong))
+
+  case class Position(value: Long, state: Int)
+  val sorted = ranges
+    .map { range =>
+      Array(Position(range._1, 0), Position(range._2, 1))
+    }
+    .flatten
+    .sortBy { case Position(v, s) => (v, s) }
+
+  case class State(
+      mergedRangeStart: Long,
+      numOfRangesInMerge: Int,
+      sumOfIngridients: Long
+  )
+  val result = sorted.foldLeft(State(0L, 0, 0L)) { case (acc, position) =>
+    position.state.match {
+      case 0 =>
+        State(
+          if (acc.numOfRangesInMerge == 0L) position.value
+          else acc.mergedRangeStart,
+          acc.numOfRangesInMerge + 1,
+          acc.sumOfIngridients
+        )
+      case 1 =>
+        State(
+          acc.mergedRangeStart,
+          acc.numOfRangesInMerge - 1,
+          if (acc.numOfRangesInMerge - 1 == 0)
+            acc.sumOfIngridients + position.value - acc.mergedRangeStart + 1
+          else acc.sumOfIngridients
+        )
+    }
+  }
+
+  result.sumOfIngridients
