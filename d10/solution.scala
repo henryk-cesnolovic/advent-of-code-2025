8271f0d90b3b63b706a501e0bf2848161810028c
Henryk Cesnolovic <unxe@Henryks-MacBook-Pro.local>
2025-12-28 21:29:45 +0200

    Day 10 solution Part 2

--- d10/solution.scala
+++ d10/solution.scala
@@ -1,5 +1,6 @@
 import scala.io.Source
 import Utils._
+import scala.annotation.init
 
 object Day10:
   @main
@@ -12,8 +13,8 @@ object Day10:
     time(solution1(dataTest))
     time(solution1(data))
 
-    // time(solution2(dataTest))
-    // time(solution2(data))
+    time(solution2(dataTest))
+    time(solution2(data))
 
   def solution1(data: List[String]): Long =
     data.map { line =>
@@ -54,4 +55,91 @@ object Day10:
       findDesired(Set(0L), 0)
     }.sum
 
-  def solution2(data: List[String]): Long = ???
+  def solution2(data: List[String]): Long =
+    data.map { line =>
+
+      val split = line.split(" ").toList
+
+      val desiredString = split.head.tail.dropRight(1)
+      val switchesString = split.tail.dropRight(1)
+      val joltages = split.last.tail.dropRight(1).split(",").map(_.toInt)
+
+      val switchesMasks = switchesString.map { switches =>
+        switches.tail.dropRight(1).split(",").foldLeft(0L) { case (acc, switch) =>
+          acc | (1 << switch.toInt)
+        }
+      }.toSet
+
+      case class State(possibleJoltages: Array[Int], presses: List[Int], multipliers: List[Int])
+      val resultStates = new scala.collection.mutable.HashSet[State]()
+      val mem = new scala.collection.mutable.HashMap[Long, List[List[Long]]]()
+
+      def findAllSolution(currentStates: Set[State]): Unit =
+        if (!currentStates.isEmpty)
+          val newStates: Set[State] = currentStates.flatMap { case State(joltages, presses, multipliers) =>
+            val multiplier = if (joltages.filter(_ % 2 != 0).isEmpty) 2 else 1
+            if (joltages.filter(_ % 2 != 0).isEmpty) {
+              (0 to joltages.size - 1).map { index => joltages(index) /= 2 }
+            }
+
+            // joltages bitmask
+            val initBitmask = joltages.reverse.foldLeft(0L) { (acc, joltage) =>
+              (acc << 1) | (joltage % 2)
+            }
+
+            val allSolutions = mem.get(initBitmask) match {
+              case Some(value) => value
+              case None =>
+                val subsets = (0 to switchesMasks.size).flatMap(switchesMasks.toList.combinations).toList
+                val solutions = subsets
+                  .map { subset =>
+                    (subset.foldLeft(initBitmask) { case (acc, switch) => acc ^ switch }, subset)
+                  }
+                  .filter(_._1 == 0L)
+                  .map(_._2)
+                mem += initBitmask -> solutions
+                solutions
+            }
+
+            val states = allSolutions.map { solution =>
+              val joltagesCopy = joltages.clone()
+              solution.foreach { bitmask =>
+                var mutBitmask = bitmask
+                var index = 0
+                while (mutBitmask != 0) {
+                  if ((mutBitmask & 1) == 1)
+                    joltagesCopy(index) -= 1
+                  index += 1
+                  mutBitmask >>= 1
+                }
+              }
+              State(joltagesCopy, solution.size +: presses, multiplier +: multipliers)
+            }
+
+            states.filter { state =>
+              val joltages = state.possibleJoltages
+
+              if (joltages.filter(_ != 0).isEmpty) {
+                resultStates.add(state)
+                false
+              } else if (!joltages.filter(_ < 0).isEmpty)
+                false
+              else
+                true
+            }
+          }
+          findAllSolution(newStates)
+        else ()
+
+      findAllSolution(Set(State(joltages, List(), List())))
+
+      val numOfPresses = resultStates.map { case State(_, presses, multiplier) =>
+        var sumOfPresses = 0
+        (0 to presses.size - 1).foreach { index =>
+          sumOfPresses = (sumOfPresses + presses(index)) * multiplier(index)
+        }
+        sumOfPresses
+      }
+
+      numOfPresses.min
+    }.sum
